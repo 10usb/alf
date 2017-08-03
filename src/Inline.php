69c199a2a100e8f576f57485a7282ae999a2bcf1
Tinus Bruins <t.bruins@gmail.com>
2017-08-03 20:25:36 +0200

    Functional internal Text element

--- src/Inline.php
+++ src/Inline.php
@@ -2,12 +2,35 @@
 namespace alf;
 
 interface Inline extends Element {
+	const FLOAT_NONE	= 0;
+	const FLOAT_LEFT	= 1;
+	const FLOAT_RIGHT	= 2;
+	
+	const ALIGN_TOP		= 0;
+	const ALIGN_MIDDLE	= 1;
+	const ALIGN_BOTTOM	= 2;
+	
 	/**
-	 * 
-	 * @param \alf\Line $line
+	 * Called by the line to set offset from the begining of the line
+	 * @param number $value
 	 */
-	public function setLine($line);
-	public function setLeft();
-	public function setTop();
+	public function setLineLeft($value);
+	
+	/**
+	 * Called by the line to set offset from the top of the line
+	 * @param number $value
+	 */
+	public function setLineTop($value);
+	
+	/**
+	 * Returs the floating value of this inline element
+	 * @return integer
+	 */
 	public function getFloating();
+	
+	/**
+	 * Returns the vertical alignment of this inline element
+	 * @return intger
+	 */
+	public function getVerticalAlignment();
 }
--- src/Text.php
+++ src/Text.php
@@ -2,5 +2,152 @@
 namespace alf;
 
 class Text implements Inline {
+	/**
+	 * 
+	 * @var string
+	 */
 	private $value;
+	
+	/**
+	 * 
+	 * @var \alf\Font
+	 */
+	private $font;
+	
+	/**
+	 * 
+	 * @var string
+	 */
+	private $color;
+	
+	/**
+	 * 
+	 * @var number
+	 */
+	private $lineHeight;
+	
+	/**
+	 * 
+	 * @var number $lineLeft
+	 * @var number $lineTop
+	 */
+	private $lineLeft, $lineTop;
+	
+	/**
+	 * 
+	 * @param string $value
+	 * @param \alf\Font $font
+	 * @param string $color
+	 * @param number $lineHeight
+	 */
+	public function  __construct($value, $font, $color, $lineHeight = false){
+		$this->value		= $value;
+		$this->font			= $font;
+		$this->color		= $color;
+		$this->lineHeight	= $lineHeight;
+		$this->lineLeft		= 0;
+		$this->lineTop		= 0;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Inline::setLineLeft()
+	 */
+	public function setLineLeft($value){
+		$this->lineLeft = $value;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Inline::setLineTop()
+	 */
+	public function setLineTop($value){
+		$this->lineTop = $value;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Inline::getFloating()
+	 */
+	public function getFloating(){
+		return Inline::FLOAT_NONE;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Inline::getVerticalAlignment()
+	 */
+	public function getVerticalAlignment(){
+		return Inline::ALIGN_BOTTOM;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Element::getMarginLeft()
+	 */
+	public function getMarginLeft(){
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Element::getMarginTop()
+	 */
+	public function getMarginTop(){
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Element::getMarginRight()
+	 */
+	public function getMarginRight(){
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Element::getMarginBottom()
+	 */
+	public function getMarginBottom(){
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Element::getWidth()
+	 */
+	public function getWidth(){
+		return $this->font->getTextWith($this->value);
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Element::getHeight()
+	 */
+	public function getHeight(){
+		if($this->lineHeight!==false) return max($this->lineHeight, $this->font->getSize());
+		return $this->font->getSize();
+	}
+	
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * @see \alf\Renderable::render()
+	 */
+	public function render($canvas){
+		$canvas->setFont($this->font->getName(), $this->font->getSize());
+		$canvas->setFillColor($this->color);
+		$canvas->fillText($this->value, $this->lineLeft, $this->lineTop + $this->font->getSize());
+	}
 }
